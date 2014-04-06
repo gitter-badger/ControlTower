426e6e7b0d074c4e7a703cd2c2f0b8fe6321152b
Kenny Luong <luong97@hawaii.edu>
2014-04-05 20:53:12 -1000

    Added tester

---
+++ Tester.py
@@ -0,0 +1,11 @@
+# Control Tower Tester Program
+# Author: Kenny Luong
+#
+# Use this script as an example for ControlTower
+
+import ControlTower
+
+supervisor = ControlTower()
+
+while True:
+	supervisor.get_menu_option()
